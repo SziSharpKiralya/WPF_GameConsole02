d2c0c1daa000ff2531a7d077e5b5352d72b92e74
Petrik Diak <diak@petrik.hu>
2026-01-14 10:05:58 +0100

    Add project files.

---
+++ WPF_GameConsole/WPF_GameConsole.Game.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace WPF_GameConsole
+{
+	public partial class MainWindow
+	{
+		DispatcherTimer gameTimer;
+
+		private double birdY = 100;
+		private double velocity = 0;
+
+		private const double gravity = 0.5;
+		private const double jumpForce = -8;
+
+		private void GameIntermission()
+		{
+			if (gameTimer != null)
+			{
+				gameTimer.Stop();
+				gameTimer.Tick -= GameLoop;
+			}
+
+			birdY = 100;
+			velocity = 0;
+			state = false;
+			Bird.Visibility = Visibility.Visible;
+			spaceToStartInfo.Visibility = Visibility.Visible;
+		}
+
+
+		private void GameStart()
+		{
+			state = true;
+			Canvas.SetTop(Bird, birdY);
+			spaceToStartInfo.Visibility = Visibility.Hidden;
+			gameTimer = new DispatcherTimer();
+			gameTimer.Interval = TimeSpan.FromMilliseconds(16);
+			gameTimer.Tick += GameLoop;
+			gameTimer.Start();
+		}
+
+		private void GameJump()
+		{
+			velocity = jumpForce;
+		}
+
+		private void GameLoop(object? sender, EventArgs e)
+		{
+			velocity += gravity;
+			birdY += velocity;
+			Canvas.SetTop(Bird, birdY);
+
+			if (birdY + Bird.Height >= GameCanvas.ActualHeight + 10) { GameIntermission();}
+			if (birdY <= -10) { GameIntermission();}
+		}
+	}
+}
---
+++ WPF_GameConsole/WPF_GameConsole.UI.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WPF_GameConsole
+{
+	public partial class MainWindow
+	{
+		private void console_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			DragMove();
+		}
+
+		private void btnClose_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+
+		private void btnBack_Click(object sender, RoutedEventArgs e)
+		{
+			LoadScreen_Menu();
+		}
+
+		private void Window_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Space)
+			{
+				if (current == "Game")
+				{
+					if (state) {GameJump();}
+					else {GameStart();}
+				}
+			}
+		}
+
+		private void btnPlay_Click(object sender, RoutedEventArgs e)
+		{
+			LoadScreen_Game();
+		}
+
+		private void LoadScreen_Menu()
+		{
+			current = "Menu";
+			windowMenuTitle.Content = current;
+			screen_Menu.Visibility = Visibility.Visible;
+			screen_Game.Visibility = Visibility.Hidden;
+		}
+
+		private void LoadScreen_Game()
+		{
+			current = "Game";
+			GameIntermission();
+			Canvas.SetLeft(Bird, 20);
+			Canvas.SetTop(Bird, birdY);
+			windowMenuTitle.Content = current;
+			screen_Menu.Visibility = Visibility.Hidden;
+			screen_Game.Visibility = Visibility.Visible;
+		}
+	}
+}
